1e3c23a01d28c9f9fc27f450e06347f676b79978
sunshine2329 <seniordeveloper00001@gmail.com>
2023-08-16 14:57:26 -0400

    Complete the oauth functionality

--- src/components/buttons/OAuth.tsx
+++ src/components/buttons/OAuth.tsx
@@ -1,8 +1,36 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
 import { FcGoogle } from 'react-icons/fc'
-// React.ChangeEvent<HTMLInputElement>
+import { toast } from 'react-toastify'
+import { db } from '../../firebase'
+import { useNavigate } from 'react-router'
+
 const OAuth = () => {
-  const handleWithGoogle = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const navigate = useNavigate()
+  const handleWithGoogle = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault()
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+
+      // check for the user
+      const docRef = doc(db, 'users', user.uid)
+      const docSnap = await getDoc(docRef)
+
+      if (!docSnap.exists()) {
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp()
+        })
+      }
+      navigate('/')
+    } catch (error) {
+      toast.error('Could not authorize with Google.')
+      console.log(error)
+    }
   }
   return (
     <button
