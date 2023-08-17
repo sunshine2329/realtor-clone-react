db6cec490739dbbe84797aebc582974d88b4a0a0
sunshine2329 <seniordeveloper00001@gmail.com>
2023-08-16 21:34:52 -0400

    add the edit functionality to the profile page

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -2,16 +2,34 @@ import classNames from 'classnames'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   title: string
+  color?: 'primary' | 'secondary' | 'default'
 }
 
-const Button: React.FC<ButtonProps> = (props: ButtonProps) => {
-  const { title, className, ...rest } = props
-  const btnClass = classNames(
-    'w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800',
-    className
-  )
+const Button: React.FC<ButtonProps> = ({
+  type = 'button',
+  title,
+  className,
+  color = 'primary',
+  ...rest
+}: ButtonProps) => {
+  const btnClass =
+    'w-full  px-7 py-3 text-sm font-medium uppercase rounded shadow-md  transition duration-150 ease-in-out'
+
+  const primaryClass = 'bg-blue-600 text-white hover:bg-blue-700 active:bg-blue-800'
+  const secondaryClass = 'bg-red-600 text-white hover:bg-red-700 active:bg-red-800'
+  const defaultClass = 'bg-gray-100 text-black hover:bg-gray-200 active:bg-gray-300'
   return (
-    <button className={btnClass} {...rest}>
+    <button
+      type={type}
+      className={classNames(
+        btnClass,
+        { [primaryClass]: color === 'primary' },
+        { [secondaryClass]: color === 'secondary' },
+        { [defaultClass]: color === 'default' },
+        className
+      )}
+      {...rest}
+    >
       {title}
     </button>
   )
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 import { Input } from '../components/inputs'
-import { getAuth } from 'firebase/auth'
+import { getAuth, updateProfile } from 'firebase/auth'
 import AskAction from '../components/AskAction'
 import { useNavigate } from 'react-router-dom'
+import { Button } from '../components/buttons'
+import { toast } from 'react-toastify'
+import { doc, updateDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 
 type FormData = {
   name: string
@@ -12,6 +16,7 @@ type FormData = {
 export default function Profile() {
   const auth = getAuth()
   const navigate = useNavigate()
+  const [changeDetail, setChangeDetail] = useState<boolean>(false)
   const [formData, setFormData] = useState<FormData>({
     name: auth.currentUser?.displayName ?? '',
     email: auth.currentUser?.email ?? ''
@@ -21,27 +26,52 @@ export default function Profile() {
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData(prevState => ({ ...prevState, [e.target.id]: e.target.value }))
   }
-
   const handleEdit = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.preventDefault()
+    setChangeDetail(prevState => !prevState)
+  }
+  const handleCancelChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    setChangeDetail(prevState => !prevState)
+    setFormData({
+      name: auth.currentUser?.displayName ?? '',
+      email: auth.currentUser?.email ?? ''
+    })
   }
   const handleSignOut = (e: React.MouseEvent<HTMLSpanElement>) => {
     e.preventDefault()
     auth.signOut()
     navigate('/')
   }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log('submit')
+    try {
+      if (auth.currentUser && auth.currentUser?.displayName !== name) {
+        await updateProfile(auth.currentUser, { displayName: name })
+
+        const docRef = doc(db, 'users', auth.currentUser.uid)
+        await updateDoc(docRef, { name })
+      }
+      toast.success('Profile details updated.')
+      setChangeDetail(false)
+    } catch (error) {
+      toast.error('Could not update the profile details')
+      console.log(error)
+    }
+  }
   return (
     <>
       <section className="max-w-6xl mx-auto flex justify-center items-center flex-col px-6">
         <h1 className="text-3xl text-center mt-6 font-bold">My Profile</h1>
         <div className="w-full md:w-[50%] mt-6">
-          <form>
+          <form onSubmit={handleSubmit}>
             <Input
               type="text"
               id="name"
               value={name}
-              disabled
-              className="mb-6"
+              disabled={!changeDetail}
+              className={`mb-6 ${changeDetail && 'bg-red-200 focus:bg-red-200'}`}
               onChange={handleChange}
             />
             <Input
@@ -52,14 +82,21 @@ export default function Profile() {
               className="mb-6"
               onChange={handleChange}
             />
-            <AskAction
-              ask={{
-                description: 'Do you want to change your name?',
-                onClick: handleEdit,
-                actionName: 'Edit'
-              }}
-              other={{ onClick: handleSignOut, actionName: 'Sign out' }}
-            />
+            {changeDetail ? (
+              <div className="flex space-x-2">
+                <Button title="Apply Change" type="submit" />
+                <Button title="Cancel" onClick={handleCancelChange} color="default" />
+              </div>
+            ) : (
+              <AskAction
+                ask={{
+                  description: 'Do you want to change your name?',
+                  onClick: handleEdit,
+                  actionName: 'Edit'
+                }}
+                other={{ onClick: handleSignOut, actionName: 'Sign out' }}
+              />
+            )}
           </form>
         </div>
       </section>
